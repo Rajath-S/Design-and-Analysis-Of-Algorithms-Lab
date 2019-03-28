a4bf89888540558c5c6b34abb0579712945cffca
Rajath-S <rajaths2510@gmail.com>
2019-03-28 15:02:13 +0530

    lab 9

--- Session 9 - Heapsort/client.c
+++ Session 9 - Heapsort/client.c
@@ -1,13 +1,17 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include"session9_heapsort.h"
+#include<time.h>
+double time_elapsed(struct timespec* start,struct timespec* end);
+
 int main()
 {
 	int n;
+	struct timespec start,end;
 	/*struct timespec start1, end1;
 	double time1;*/
 	scanf("%d\n", &n);
-	Data* arr = (Data*)malloc(sizeof(Data)*(n+1));
+	Data* arr = (Data*)malloc(sizeof(Data)*(n+1));  //array indices ranges from 1 to n
 	//printf("enter elements\n");
 	for (int i = 1;i <=n;i++)       //take input
 	{
@@ -15,13 +19,22 @@ int main()
 		scanf("%d", &arr[i].value);
 	}
 	//we have to sort by id
+	clock_gettime(CLOCK_REALTIME,&start);
 	HeapSort(arr,n);
+	clock_gettime(CLOCK_REALTIME,&end);
 	for (int i = 1;i <=n;i++)       //print sorted array
 	{
 		printf("%d ", arr[i].id);
 		printf("%d\n", arr[i].value);
 	}
-	
+	printf("%lf\n",time_elapsed(&start,&end));
 	free(arr);
-
 }
+	double time_elapsed(struct timespec* start,struct timespec* end)
+{
+	double t;
+	t=end->tv_sec-start->tv_sec;
+	t+=(end->tv_nsec-start->tv_nsec)*0.000000001;
+	return t;
+}
+
--- Session 9 - Heapsort/functions.c
+++ Session 9 - Heapsort/functions.c
@@ -3,23 +3,27 @@
 #include<stdio.h>
 void swap(Data* A, int i, int j);
 void Heapify(Data* A, int n, int k);
-void HeapSort(Data* A, int n)
-{
-	void HeapBottomUp(A,n);  //construct a max heap from an unsorted array
-	for (int i = n;i <= 2;i--)
-	{
-		swap(A,1,i);
-		Heapify(A, i - 1, 1); // We shrink the size by one
-	}
-}
 //building a heap
 void HeapBottomUp(Data * A, int n)   //note that this function takes o(n) time
 {
 	if (n <= 1)
 		return;
-	for (int i = n / 2;i >= 1;i--)
+	for (int i = n / 2;i >= 1;i--)    // all elements fwith indices n/2 to 1 are parents
 		Heapify(A, n, i);
 }
+
+
+
+void HeapSort(Data* A, int n)
+{
+	HeapBottomUp(A,n);  //construct a max heap from an unsorted array
+	for (int i = n;i >= 2;i--)   
+	{
+		swap(A,1,i);
+		Heapify(A, i - 1, 1); // We shrink the size by one
+	}
+}
+
 void swap(Data* A, int i, int j)
 {
 	Data temp;
@@ -34,14 +38,13 @@ void Heapify(Data* A, int n, int k)   //k is the root from which if has to heapi
 	int j = 2 * k;//j is index of left child
 	if (j + 1 <= n)//check if right child exists
 	{
-		if (A[j + 1].id > A[j].id)
-		{
+		if (A[j + 1].id > A[j].id)    //if right child is greater than left child
 			j = j + 1;
-		}
-		if (A[j].id > A[k].id)
-		{
+	}
+	if (A[j].id > A[k].id)   //if child is greater than parent then interchange
+	{
 			swap(A, j, k);
 			Heapify(A, n, j);
-		}
 	}
+	
 }
