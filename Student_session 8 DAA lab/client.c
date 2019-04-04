b52375e45bdfdcde43bb2d38317182c5829543ca
Rajath-S <rajaths2510@gmail.com>
2019-04-04 15:20:27 +0530

    after lab 10, 11

--- Student_session 8 DAA lab/client.c
+++ Student_session 8 DAA lab/client.c
@@ -1,10 +1,11 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include"session8_dfsbfs.h"
-
-
+#include<time.h>
+#include<math.h>
+#define nano 1000000000L
 int main()
-{
+{struct timespec start,end;
 	int dfs = 0;
   printf("starting function\n");
   int testcases,numOfNodes;
@@ -27,23 +28,31 @@ int main()
 			scanf("%d",&(x[i][j]));
 		}
 	}
-	printf("input taken\n");
-	for(int i = 0; i <numOfNodes; i++)
-	{
-		for(int j = 0;j <numOfNodes; j++)
-		{
-			printf("%d",(x[i][j]));
-		}
-		printf("\n");
-	}
+	// printf("input taken\n");
+	// for(int i = 0; i <numOfNodes; i++)
+	// {
+	// 	for(int j = 0;j <numOfNodes; j++)
+	// 	{
+	// 		printf("%d",(x[i][j]));
+	// 	}
+	// 	printf("\n");
+	// }
 	//printf("\n");
 	int numcomp;
 	printf("computing number of components...\n");
+	clock_gettime(CLOCK_REALTIME,&start);
 	if (dfs)
 		numcomp = count_components_dfs(x, numOfNodes);
 	else
 		numcomp = count_components_dfs(x, numOfNodes);
+	clock_gettime(CLOCK_REALTIME,&end);
+	double time_=(end.tv_sec-start.tv_sec)+(end.tv_nsec-start.tv_nsec)*pow(10, -9);
 	printf("number of components=%d\n",numcomp);
+	if(dfs=1)
+		printf("time taken for dfs= %lf \n",time_);
+	else
+		printf("time taken for bfs= %lf \n",time_);
+
 	for(int i=0;i<numOfNodes;i++)
 	{
 		free(x[i]);
